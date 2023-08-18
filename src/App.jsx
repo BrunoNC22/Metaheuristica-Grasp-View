a45a4202c4741e149cc5e20cc3803628b22c5af2
BrunoNC22 <thebegone2@gmail.com>
2023-08-18 00:16:10 -0300

    feat: app.jsx updade

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,116 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+async function getText() {
+  const url =
+    "https://metaheuristica-grasp-api-production.up.railway.app/teste";
+  const options = {
+    method: "GET",
+  };
+  try {
+    const response = await fetch(url, options);
+    return response;
+  } catch (error) {
+    console.error("Error:", error);
+    throw new Error("An error occurred while making the GET request.");
+  }
+}
+
+async function createObj(text) {
+  const graph = { nome: text };
+
+  const url =
+    "https://metaheuristica-grasp-api-production.up.railway.app/teste";
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(graph),
+  };
+  try {
+    const response = await fetch(url, options);
+    return response;
+  } catch (error) {
+    console.error("Error:", error);
+    throw new Error("An error occurred while making the GET request.");
+  }
+}
+
+async function showText() {
+  const apiText = document.getElementById("api-text");
+  const graph = {};
+  try {
+    const resp = await getText();
+    if (resp.status === 200) {
+      graph.value = await resp.json();
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    throw new Error("An error occurred while making the GET request.");
+  }
+
+  apiText.textContent = graph.value.nome;
+}
+
+async function getObjText() {
+  const inputText = document.getElementById("input-text");
+  const text = document.getElementById("api-new-obj-response");
+  const graph = {};
+
+  try {
+    const resp = await createObj(inputText.value);
+    if (resp.status === 200) {
+      graph.value = await resp.json();
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    throw new Error("An error occurred while making the GET request.");
+  }
+
+  inputText.value = "";
+  text.textContent = graph.value.nome;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+        {/* 
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        */}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <h1>Metaheuristica GRASP</h1>
+      <div className="conteudo">
+        <button onClick={showText} className="button test-response-button">
+          Texto retirado da api:
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <div className="test-response">
+          <p id="api-text"></p>
+        </div>
+
+        <input
+          id="input-text"
+          className="input"
+          placeholder="Nome do objeto"
+          type="text"
+        ></input>
+        <button className="button test-response-button" onClick={getObjText}>
+          Criar objeto na api
+        </button>
+
+        <div className="obj-response">
+          <p id="api-new-obj-response"></p>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
